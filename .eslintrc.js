caa3fc4e436a062d67a5575d035dd18ca877d3e4
Callum Silcock <github@csi.lk>
2020-04-22 09:09:48 +0100

    ci: add eslint / prettier dependencies and config
    keep a consistent formatting style, speed up development

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  plugins: [
+    "import",
+    "json",
+    "unicorn",
+    "sort-imports-es6-autofix",
+    "jest-dom",
+  ],
+  extends: [
+    "airbnb-base",
+    "plugin:unicorn/recommended",
+    "prettier",
+    "prettier/unicorn",
+  ],
+}
