c88f68cd0fcaf13a5baaf83bb103d436659b59fb
Callum Silcock <github@csi.lk>
2020-04-22 11:42:52 +0100

    style: added reset

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
-console.log('Welcome to the Which? Javascript exercise!');
-console.log('If you are reading this, your Javascript runtime is all up and running correctly.');
+import app from './app'
+import './reset.css'
+import './styles.css'
 
-// dont put any source in this file, this is just an entry point for the app.
-// you can require things in.
-import app from './app';
-import './styles.css';
-
-app();
+app()
