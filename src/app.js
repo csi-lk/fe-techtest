513750b279d3c0a0db8cf672492f6ef901a3a836
Callum Silcock <github@csi.lk>
2020-04-22 10:36:16 +0100

    feat(app): now generates buttons to the config range

--- src/app.js
+++ src/app.js
@@ -1,6 +1,16 @@
-// source goes here
-const app = function() {
-  console.log('hello from app!');
-};
+import CONFIG from './config'
+import generateArrayFromRange from './lib/generate-array-from-range'
 
-export default app;
+const createButtons = range =>
+  generateArrayFromRange(range).map(number => {
+    const button = document.createElement('button')
+    button.type = 'button'
+    button.textContent = number
+    return button
+  })
+
+const app = () => {
+  document.querySelector('main').append(...createButtons(CONFIG.RANGE))
+}
+
+export default app
